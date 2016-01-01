2670e5000a12eb532fad1a870a0478df0c742b14
Spencer <pallyoung@163.com>
2016-01-02 00:58:53 +0800

    ios

---
+++ Lost_ios/index.ios.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+//" CREATE TABLE IF NOT EXISTS CONTACTS(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT,PHONE TEXT)";
+var React = require('react-native');
+var CountDown = require('./CountDown');
+
+
+var {
+  Component,
+  AppRegistry
+} = React;
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+  }
+  render(){
+    return (
+      <CountDown
+      />
+    )
+  }
+}
+
+
+
+AppRegistry.registerComponent('groups', () => App);
+
+module.exports = App;
