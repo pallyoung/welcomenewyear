442d5dc999c9e280eaa5f2d6646d2c3059bc7d75
shyeshaopeng <yeshaopeng@corp.netease.com>
2019-03-11 16:59:53 +0800

    添加ts支持

--- lost/App.tsx
+++ lost/App.tsx
@@ -7,7 +7,8 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
+import React from 'react';
+import { Component } from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
@@ -17,13 +18,18 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export interface Props {
+
+}
+interface State {
+  enthusiasmLevel: number
+}
+export default class App extends Component<Props,State> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>To get started333, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
